15769529ec853497ead348eaf1488590db22b80b
plphuc <phamluumyphuc@gmail.com>
2023-08-27 14:41:15 +0700

    update

--- assets/js/getRequests.js
+++ assets/js/getRequests.js
@@ -1,14 +1,14 @@
 export async function getBestSellerProducts(args) {
   const params = {...args, size: args.size || 6}
   const paramsString = new URLSearchParams(params).toString() 
-  const result = await fetch(`http://localhost:3000/bestSeller?${paramsString}`)
+  const result = await fetch(`https://shopeemockapi-1-t9661588.deta.app/bestSeller?${paramsString}`)
   return result.json()
 }
 
 export async function getSuggestedProducts(args) {
   const params = {...args, size: args.size || 6}
   const paramsString = new URLSearchParams(params).toString() 
-  const result = await fetch(`http://localhost:3000/suggested?${paramsString}`)
+  const result = await fetch(`https://shopeemockapi-1-t9661588.deta.app/suggested?${paramsString}`)
   return result.json()
 }
 
@@ -16,13 +16,13 @@ export async function getProducts(args) {
   const params = {...args, page: args.page || 1, size: args.size || 20}
   const paramsString = new URLSearchParams(params).toString() 
 
-  const result = await fetch(`http://localhost:3000/products?${paramsString}`)
+  const result = await fetch(`https://shopeemockapi-1-t9661588.deta.app/products?${paramsString}`)
   return result.json()
 }
 
 export async function getSearchResult(args) {
   const params = {...args}
   const paramsString = new URLSearchParams(params).toString() 
-  const result = await fetch(`http://localhost:3000/search?${paramsString}`)
+  const result = await fetch(`https://shopeemockapi-1-t9661588.deta.app/search?${paramsString}`)
   return result.json()
 }
